7878f9a109f1b3a075535056c26e52faa4fcca6d
Michael van Acken <michael.van.acken@gmail.com>
2023-06-10 06:54:30 +0200

    refactor: extract-publics via NmspIO
    Rename tcljc.publics to tcljc.main.publics and remove its dependency
    on ctx.

--- src/tinyclj.compiler/module-info.java
+++ src/tinyclj.compiler/module-info.java
@@ -5,18 +5,18 @@ module tinyclj.compiler {
   exports tcljc.main;
   
   exports tcljc.classfile;
-  exports tcljc.compiler.branch;
   exports tcljc.compiler;
+  exports tcljc.compiler.branch;
   exports tcljc.compiler.conv;
   exports tcljc.compiler.resolve;
   exports tcljc.compiler.sigclass;
   exports tcljc.compiler.sigfn;
   exports tcljc.config;
   exports tcljc.context;
+  exports tcljc.emitter;
   exports tcljc.emitter.beachhead;
   exports tcljc.emitter.bytecode;
   exports tcljc.emitter.classes;
-  exports tcljc.emitter;
   exports tcljc.emitter.emitfn;
   exports tcljc.emitter.exprcode;
   exports tcljc.emitter.namespace;
@@ -29,11 +29,14 @@ module tinyclj.compiler {
   exports tcljc.macro;
   exports tcljc.main.builder;
   exports tcljc.main.efmt;
+  exports tcljc.main.invoke;
   exports tcljc.main.options;
+  exports tcljc.main.publics;
   exports tcljc.main.tclj0;
+  exports tcljc.main.tclj1;
+  exports tcljc.main.watcher;
   exports tcljc.parser;
   exports tcljc.predefined;
-  exports tcljc.publics;
   exports tcljc.reader;
   exports tcljc.runtime;
   exports tcljc.synquote;
